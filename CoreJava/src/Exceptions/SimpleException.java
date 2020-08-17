9443b7019f466a2484e27b7334ab3f58f06a792f
TunayNovruz <tunaynovruz@gmail.com>
2020-08-17 10:20:09 +0400

    Gc and exception test

---
+++ CoreJava/src/Exceptions/SimpleException.java
@@ -0,0 +1,28 @@
+package Exceptions;
+
+public class SimpleException {
+    public static void main(String[] args) {
+        var a = 0;
+        var b= 7;
+
+        try {
+            System.out.println(b / a);
+        }catch (ArrayIndexOutOfBoundsException | ArithmeticException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("works always");
+        }
+        System.out.println("cant reach this if dont use try");
+
+        try {
+            testEx();
+        }catch (Exception e){
+            //do something
+        }
+    }
+
+    public static void testEx () throws Exception{
+        System.out.println("zzz");
+        throw new Exception("exception message",new ArithmeticException("this is cause"));
+    }
+}
---
+++ CoreJava/src/GC/GarbageCollector.java
@@ -0,0 +1,58 @@
+package GC;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.Random;
+
+public class GarbageCollector {
+    public static void main(String[] args) {
+        visualVmTest();
+        //getGCInfo();
+    }
+
+    public static void getGCInfo() {
+        List<GarbageCollectorMXBean> list = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean bean : list) {
+            System.out.println(bean.getName());
+            System.out.println(bean.getCollectionCount());
+            System.out.println(bean.getCollectionTime());
+            for (String name : bean.getMemoryPoolNames()) {
+                System.out.println(name);
+            }
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+            }
+            System.out.println("-------------------------");
+        }
+    }
+
+    public static void visualVmTest() {
+        bigObj objs[] = new bigObj[10000000];
+        for (int i = 0; i <10000000 ; i++) {
+            objs[i] = new bigObj();
+            var a = new Random();
+            try{
+                Thread.sleep(1);
+            }catch (Exception e){}
+        }
+    }
+}
+
+
+class bigObj{
+    long a;
+    long aa;
+    long aaa;
+    long aassa;
+    long aassdda;
+    long aassdddda;
+    long aassdddddda;
+    long aassdddddddda;
+    long aassdddddddddda;
+    long aassdddddddddddda;
+    long aassddddddddddddddda;
+    long aassdddddddddddddda;
+    long aassddddddddddddda;
+}
